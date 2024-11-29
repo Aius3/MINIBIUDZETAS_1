75a13c6a3fec96e9d54f4a88dd9f29e0acffa0a9
Aius3 <kajus.mocevicius@gmail.com>
2024-11-29 10:32:06 +0200

    Sukurtas pajamų įvedimas į sąrašą

--- Minibiudzetas_1.py
+++ Minibiudzetas_1.py
@@ -1,12 +1,21 @@
-print("Pagrindinis meniu: \n"
-      "1. Įvesti pajamas\n"
-      "2. Įvesti išlaidas\n"
-      "3. Atspausdinti pajamų eilutes\n"
-      "4. Atspausdinti išlaidų eilutes\n"
-      "5. Atspausdinti statistiką\n"
-      "q - išeiti\n"
-      "")
+from datetime import datetime
 
-pagr_meniu_pasirinkimas = input("> ")
+pajamos = []
+islaidos = []
 
+while True:
+    print("Pagrindinis meniu: \n"
+          "1. Įvesti pajamas\n"
+          "2. Įvesti išlaidas\n"
+          "3. Atspausdinti pajamų eilutes\n"
+          "4. Atspausdinti išlaidų eilutes\n"
+          "5. Atspausdinti statistiką\n"
+          "q - išeiti\n")
+    pagr_meniu_pasirinkimas = input("> ")
+
+    if pagr_meniu_pasirinkimas == "1":
+        paj_data = input("Įveskite datą: ")
+        paj_pav = input("Įveskite pajamų pavadinimą: ")
+        paj_suma = input("Įveskite pajamų sumą: ")
+        pajamos.append([paj_data, paj_pav, paj_suma])
 
