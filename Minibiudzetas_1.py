27a148abf841b969df35566bd23aeedee1b9c661
Aius3 <kajus.mocevicius@gmail.com>
2024-11-29 10:09:51 +0200

    Sukuriamas meniu

---
+++ Minibiudzetas_1.py
@@ -0,0 +1,12 @@
+print("Pagrindinis meniu: \n"
+      "1. Įvesti pajamas\n"
+      "2. Įvesti išlaidas\n"
+      "3. Atspausdinti pajamų eilutes\n"
+      "4. Atspausdinti išlaidų eilutes\n"
+      "5. Atspausdinti statistiką\n"
+      "q - išeiti\n"
+      "")
+
+pagr_meniu_pasirinkimas = input("> ")
+
+
